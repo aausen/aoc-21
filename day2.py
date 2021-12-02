ccaaa80faf441b865a0144403f6d3600c8912fcb
Annie Ausen <ausen.annie@gmail.com>
2021-12-02 14:27:05 -0600

    finished part 2

--- day2.py
+++ day2.py
@@ -16,3 +16,25 @@ def find_position_product():
         elif orientation == "up":
             depth -= int(num)
     print("Result = ", horizontal_position * depth)
+    return horizontal_position * depth
+
+
+def find_depth_with_aim():
+    horizontal_position = 0
+    depth = 0
+    aim = 0
+
+    for direction in directions:
+        split_directions = direction.strip().split(" ")
+        orientation, num = split_directions
+        num = int(num)
+        
+        if orientation == "forward":
+            horizontal_position += num
+            depth = depth + (aim * num)
+        elif orientation == "down":
+            aim += num
+        elif orientation == "up":
+            aim -= num
+    print("Result = ", horizontal_position * depth)
+    return horizontal_position * depth
